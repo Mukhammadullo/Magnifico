848787c183635f5c4742459222cf5be0d6907b78
Muhammadullo <mnastulloev@gmail.com>
2023-12-13 11:03:19 +0500

    header-fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <div className='w-[100%] h-[12vh] border-solid border-purple-800 border-[2px] flex justify-between items-center'>
           <h1 className='font-bold text-[26px] ml-[10px]'>magnifico</h1>
 
-          <ul className='flex'>
+          <ul className='flex '>
             <li className='font-bold text-[14px] m-[15px]'>Products</li>
             <li className='font-bold text-[14px] m-[15px]'>Solutions</li>
             <li className='font-bold text-[14px] m-[15px]'>Services</li>
@@ -26,7 +26,7 @@ function App() {
           </ul>
 
           <div className='mr-[10px]'>
-            <button className='w-[100px]  h-[7vh]    text-[#2563EB]'>Log In</button>
+            <button className='w-[100px]  h-[7vh]  text-[#2563EB]'>Log In</button>
             <button className=' w-[150px] h-[7vh] text-[white] rounded-[12px] bg-[#2563EB]'>Sign Up Free</button>
           </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'pl': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'dekstop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
