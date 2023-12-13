043a5541a31af308eb4a2e3f6306e4cfcc27a911
Muhammadullo <mnastulloev@gmail.com>
2023-12-13 10:41:17 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,23 @@ function App() {
       {/*# header */}
       <header className='w-[100%] h-[100vh]  border-solid border-red-500 border-[2px]'>
         {/* navbar */}
-        <div className='w-[100%] h-[12vh] border-solid border-purple-800 border-[2px]'>
-          <h1>magnifico</h1>
+        <div className='w-[100%] h-[12vh] border-solid border-purple-800 border-[2px] flex justify-between items-center'>
+          <h1 className='font-bold text-[26px] ml-[10px]'>magnifico</h1>
+
+          <ul className='flex'>
+            <li className='font-bold text-[14px] m-[15px]'>Products</li>
+            <li className='font-bold text-[14px] m-[15px]'>Solutions</li>
+            <li className='font-bold text-[14px] m-[15px]'>Services</li>
+            <li className='font-bold text-[14px] m-[15px]'>Help Center</li>
+            <li className='font-bold text-[14px] m-[15px]'>Pricing</li>
+          </ul>
+
+          <div>
+            <button>Log In</button>
+            <button>Sign Up Free</button>
+          </div>
+
+
         </div>
 
 
